6234c7e9acaa689451e792a2d21d6840faa7abac
Richard Davis <rdavis.bacs@gmail.com>
2015-06-08 13:39:37 -0400

    load links from different files

--- sagan.rb
+++ sagan.rb
@@ -1,24 +1,42 @@
 require "csv"
 require "sinatra"
 require "uri"
+require "pry"
+
+def at_work?
+  t = Time.now
+  start_time = Time.local(t.year, t.month, t.day, 7, 30)
+  end_time = Time.local(t.year, t.month, t.day, 17, 30)
+
+  if (1..5).include?(t.wday) && t.between?(start_time, end_time)
+    return true
+  end
+
+  return false
+end
+
+def csv_file
+  if at_work?
+    return "work.csv"
+  else
+    return "home.csv"
+  end
+end
 
 def links
   result = []
+  filename = csv_file
   csv_options = {
     col_sep: "; ",
     headers: true,
     header_converters: :symbol
   }
-  CSV.foreach("links.csv", csv_options) do |row|
+  CSV.foreach(filename, csv_options) do |row|
     link = row.to_hash
     link[:hostname] = URI(link[:url]).hostname
     result << link
   end
-  result
-end
-
-def sorted_links
-  links.sort_by { |link| link[:title].downcase }
+  result.sort_by { |link| link[:title].downcase }
 end
 
 get "/" do
@@ -26,5 +44,5 @@ get "/" do
 end
 
 get "/links" do
-  erb :links, locals: { links: sorted_links }
+  erb :links, locals: { links: links }
 end
